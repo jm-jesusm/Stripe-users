6dea9b1e55a14cecc6a995248508564cbadaabc8
Jesus Martinez Garcia <p-jmartinez@neoris.com>
2023-04-18 18:28:10 +0200

    Eliminación de tests

--- src/test/java/neoris/crud/stripe/StripeApplicationTests.java
+++ src/test/java/neoris/crud/stripe/StripeApplicationTests.java
@@ -1,13 +1,8 @@
 package neoris.crud.stripe;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StripeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
